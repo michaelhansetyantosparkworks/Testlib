aa112dea4560419843eecaa92fbf9d2b56fd2b06
Michael Hansetyanto <michael.hansetyanto@sparkworks.co.id>
2022-03-21 15:40:42 +0700

    Update

---
+++ Testlib/Classes/MainThirdPartyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainThirdPartyViewController.swift
+//  Testlib
+//
+//  Created by Sparkworks on 2022/03/21.
+//
+
+import Foundation
+
+import UIKit
+
+class MainThirdPartyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
